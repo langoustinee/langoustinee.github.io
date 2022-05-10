9c439ae0ff6c360a40cabac4a797e5a347ae663a
jooney-95 <xmun777@gmail.com>
2022-05-11 00:48:59 +0900

    blog post init

---
+++ _posts/2022-05-10-first-post.md
@@ -0,0 +1,16 @@
+--- 
+title: "벡엔드 개발자란?(작성중)" 
+
+excerpt: "GitHub Blog에서 포스팅을 시작하기로 하였다." 
+
+categories: 
+- Blog 
+
+tags: 
+- [Blog, Github, Backend, developer]
+
+toc: true
+toc_sticky: true
+
+date: 2022-05-10
+--- 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "벡엔드 개발자란?(작성중)",
+        "excerpt":" ","categories": ["Blog"],
+        "tags": ["Blog","Github","Backend","developer"],
+        "url": "/blog/first-post/",
+        "teaser": null
+      }]
