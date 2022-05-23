965b2e0222ae28f3125f323c9caf75a21998a6db
jooney-95 <xmun777@gmail.com>
2022-05-23 17:38:44 +0900

    s

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "벡엔드 개발자란?(작성중)",
-        "excerpt":" ","categories": ["Blog"],
+        "excerpt":"나는 왜 백엔드 개발자가 되려고 하는걸까?  ","categories": ["Blog"],
         "tags": ["Blog","Github","Backend","developer"],
         "url": "/blog/first-post/",
         "teaser": null
@@ -8,6 +8,6 @@ var store = [{
         "title": "[알고리즘 스터디] 백준 2588번",
         "excerpt":"코딩테스트 준비를 위해 백준에서 알고리즘 공부를 시작했다. 1단계 사칙연산 풀이의 마지막 문제인 2588번 곱셈문제 풀이를 기록한다. (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오. 문제: 입력받은 세자리수의 인덱스를 찾아서 곱해줘야 하는데 어떻게 인덱싱을 하여 곱해줄지가 문제였다. 해결: 반복문을 통해 두번째 세자리수(B)를...","categories": ["Blog"],
         "tags": ["Blog","algorithm","study","Backjoon"],
-        "url": "/blog/first-post/",
+        "url": "/blog/second-post/",
         "teaser": null
       }]
