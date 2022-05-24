59ac8807d8adb3ef8f061d3867973d608159bf2a
jooney-95 <xmun777@gmail.com>
2022-05-25 00:00:00 +0900

    s

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -16,4 +16,10 @@ var store = [{
         "tags": ["Blog","algorithm","study","Backjoon"],
         "url": "/blog/3-post/",
         "teaser": null
+      },{
+        "title": "[알고리즘 스터디] 백준 2525번",
+        "excerpt":"2525번 오븐시계 첫째 줄에는 현재 시각이 나온다. 현재 시각은 시 A (0 ≤ A ≤ 23) 와 분 B (0 ≤ B ≤ 59)가 정수로 빈칸을 사이에 두고 순서대로 주어진다. 두 번째 줄에는 요리하는 데 필요한 시간 C (0 ≤ C ≤ 1,000)가 분 단위로 주어진다. 첫째 줄에 종료되는 시각의...","categories": ["Blog"],
+        "tags": ["Blog","algorithm","study","Backjoon"],
+        "url": "/blog/4-post/",
+        "teaser": null
       }]
