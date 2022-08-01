4c98255b989d6c534548614028d4709f8769d2fe
lango <xmun777@naver.com>
2022-08-01 17:37:34 +0900

    [blog] 2022-08-01 post update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -71,7 +71,7 @@ var store = [{
         "url": "/python/12-post/",
         "teaser": null
       },{
-        "title": "[Java] 백준 10951번",
+        "title": "[알고리즘 스터디 - Java] 백준 10951번",
         "excerpt":"A+B - 4 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) Input-1) 1 1 2 3 3 4 9 8 5 2 Output-1)...","categories": ["Java"],
         "tags": ["Blog","algorithm","study","java","Backjoon"],
         "url": "/java/12-post/",
