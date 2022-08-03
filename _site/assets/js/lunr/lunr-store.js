586a11be0d07f4bb14db00416000c1a3b1150266
lango <xmun777@naver.com>
2022-08-04 00:06:48 +0900

    [solving] 2022-08-03 backjoon-4673 solve

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -94,4 +94,10 @@ var store = [{
         "tags": ["Blog","algorithm","study","java","Backjoon"],
         "url": "/java/16-post/",
         "teaser": null
+      },{
+        "title": "[알고리즘 스터디 - Java] 백준 4673번",
+        "excerpt":"Output 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 1 3 5 7 9 20 31 42 53 64 | | &lt;-- a lot more numbers | 9903 9914 9925 9927 9938 9949 9960 9971...","categories": ["Java"],
+        "tags": ["Blog","algorithm","study","java","Backjoon"],
+        "url": "/java/17-post/",
+        "teaser": null
       }]
