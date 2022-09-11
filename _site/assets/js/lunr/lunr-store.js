433509cf53a96a7d81894e294584f120af58b918
lango <xmun777@naver.com>
2022-09-11 23:17:15 +0900

    [프로그래머스] 2022-09-11 level1 로또의 최고순위와 최저순위 문제 풀이

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -322,4 +322,10 @@ var store = [{
         "tags": ["blog","algorithm","study","java","programmers"],
         "url": "/java/54-post/",
         "teaser": null
+      },{
+        "title": "[Java] 프로그래머스(level-1) - 로또의 최고 순위와 최저 순위",
+        "excerpt":"제한사항 lottos는 길이 6인 정수 배열입니다. lottos의 모든 원소는 0 이상 45 이하인 정수입니다. 0은 알아볼 수 없는 숫자를 의미합니다. 0을 제외한 다른 숫자들은 lottos에 2개 이상 담겨있지 않습니다. lottos의 원소들은 정렬되어 있지 않을 수도 있습니다. win_nums은 길이 6인 정수 배열입니다. win_nums의 모든 원소는 1 이상 45 이하인 정수입니다. win_nums에는...","categories": ["Java"],
+        "tags": ["blog","algorithm","study","java","programmers"],
+        "url": "/java/55-post/",
+        "teaser": null
       }]
