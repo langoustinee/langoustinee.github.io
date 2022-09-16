81f1138c19c747641ec3674956fb4bb748a8d2b1
lango <xmun777@naver.com>
2022-09-16 17:29:55 +0900

    [백준] 2022-09-16 실버2 1874번 스택 수열 문제 풀이

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -395,6 +395,12 @@ var store = [{
         "url": "/java/66-post/",
         "teaser": null
       },{
+        "title": "[Java] 백준 1874번 - 스택 수열",
+        "excerpt":"입출력 예시 Input 8 4 3 6 8 7 5 2 1 Output + + + + - - + + - + + - - - - - 문제 풀이 문제 제목에도 나와있듯이, 자료구조중 하나인 스택(Stack)을 활용해야 한다. 입력값마다 스택에 push 및 pop을 하여 수열을 만들 수 있는지 검증해야...","categories": ["Java"],
+        "tags": ["blog","algorithm","study","java","baekjoon"],
+        "url": "/java/69-post/",
+        "teaser": null
+      },{
         "title": "[Java] 프로그래머스(level-2) - 구명보트",
         "excerpt":"입출력 예시 Input-1 people = [70, 50, 80, 50] limit = 100 Output-1 3 Input-2 people = [70, 80, 50] limit = 100 Output-2 3 문제 풀이 먼저, 보트에는 단 2명만 탈 수 있다는 점을 유의해야 한다. 문제를 풀기 위한 아이디어 자체는 간단하다. 초안 아이디어 무게제한/2 을 넘는 사람이면, 한...","categories": ["Java"],
         "tags": ["blog","algorithm","study","java","programmers"],
