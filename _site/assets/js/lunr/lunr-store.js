a192b0b4a3a5d972ae3739ca88af6ac95094ae7e
lango <xmun777@naver.com>
2022-10-20 23:58:18 +0900

    백준: 1436번 문제 풀이

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -646,4 +646,10 @@ var store = [{
         "tags": ["blog","algorithm","study","java","baekjoon"],
         "url": "/java/108-post/",
         "teaser": null
+      },{
+        "title": "[Java] 백준(실버-4) 1436번 - 영화감독 슘",
+        "excerpt":"문제 풀이 작성코드 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 import java.io.*; import java.util.*;...","categories": ["Java"],
+        "tags": ["blog","algorithm","study","java","baekjoon"],
+        "url": "/java/109-post/",
+        "teaser": null
       }]
