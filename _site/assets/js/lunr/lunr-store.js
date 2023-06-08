d827bc54df1c3a1d888fc6d365d91f6dec7d2495
lango <xmun777@naver.com>
2023-06-08 13:28:37 +0900

    백준: 9184번 문제풀이

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1018,4 +1018,10 @@ var store = [{
         "tags": ["blog","algorithm","study","java","baekjoon"],
         "url": "/java/169-post/",
         "teaser": null
+      },{
+        "title": "[Java] 백준(실버-2) 9184번 - 신나는 함수 실행",
+        "excerpt":"문제 분석 작성코드 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47...","categories": ["Java"],
+        "tags": ["blog","algorithm","study","java","baekjoon"],
+        "url": "/java/170-post/",
+        "teaser": null
       }]
